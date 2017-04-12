f151775fe57557e68ced16bb67e544fa4d42469e
Paul Newman <Paul Newman>
2017-04-12 00:31:13 -0600

    Fixed menu to highlight correctly when in Work taxonomy and Blog taxonomy. Made back-to-top button only appear when scrolling up.

--- docs/js/totop.js
+++ docs/js/totop.js
@@ -1,16 +1,19 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("to-top").style.opacity = 1 ;
-    } else {
-        document.getElementById("to-top").style.opacity = 0 ;
-    }
-}
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
+
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function(){  
+   var st = window.pageYOffset || document.documentElement.scrollTop;  
+   if (st > lastScrollTop){
+       document.getElementById("to-top").style.bottom = "-100%";
+       document.getElementById("to-top").style.opacity = 0 ;
+   } else {
+      document.getElementById("to-top").style.bottom = "25px";
+      document.getElementById("to-top").style.opacity = 1 ;
+   }
+   lastScrollTop = st;
+}, false);
